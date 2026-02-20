d23db3b7cf48f0ccdae94ebb2d21d532368aca67
Renan Pagani <r3nv.dev@gmail.com>
2026-02-20 20:14:47 -0300

    chore: remove dead Fluent/SQLite/PostgreSQL code
    Removes unused database backends (SQLiteSessionRepository,
    PostgreSQLSessionRepository, configure.swift) and their Package.swift
    dependencies. JSONFileSessionRepository is the only persistence backend.

--- Package.swift
+++ Package.swift
@@ -5,18 +5,12 @@ let package = Package(
     name: "ZeloMCP",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
             ],
             path: "Sources/App"
         ),
